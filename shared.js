dfff7f82884d72d68ddbe840d5b5b1fbd30355bd
Tiago Ceccon <ceccon.t@hotmail.com>
2021-01-01 22:04:23 -0300

    Add side navigation with hamburger button

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ let modal = document.querySelector('.modal');
 let backdrop = document.querySelector('.backdrop');
 let modalNoButton = document.querySelector('.modal__action--negative');
 let selectPlanButtons = document.querySelectorAll('.plan button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
 let closeModal = () => {
     modal.style.display = 'none';
@@ -15,5 +17,13 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 modalNoButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
