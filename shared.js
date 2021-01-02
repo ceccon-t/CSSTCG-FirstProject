1ee13e7244e35d703a56ae7291dd09d98e958b57
Tiago Ceccon <ceccon.t@hotmail.com>
2021-01-01 21:44:57 -0300

    Open and close modal dynamically

--- shared.js
+++ shared.js
@@ -1,6 +1,12 @@
 let modal = document.querySelector('.modal');
 let backdrop = document.querySelector('.backdrop');
+let modalNoButton = document.querySelector('.modal__action--negative');
 let selectPlanButtons = document.querySelectorAll('.plan button');
+
+let closeModal = () => {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+};
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
@@ -8,3 +14,6 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
         backdrop.style.display = 'block';
     });
 }
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
