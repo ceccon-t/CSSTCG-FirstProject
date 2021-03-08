94f3d7128dae01cd71749f4c86bcaa6b538d5422
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-08 00:17:45 -0300

    Style backdrop with transition

--- shared.js
+++ shared.js
@@ -13,6 +13,9 @@ let closeModal = () => {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 200);
 };
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
@@ -20,7 +23,10 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
         // modal.style.display = 'block';
         // backdrop.style.display = 'block';
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(function() {
+            backdrop.classList.add('open')
+        }, 10);
     });
 }
 
@@ -38,5 +44,8 @@ toggleButton.addEventListener('click', () => {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
 })
